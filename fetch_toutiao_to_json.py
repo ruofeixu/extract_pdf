b7a43ffe0ab2400f9e9cf7f1023d96213fa80ff6
xuke <764408187@qq.com>
2019-09-12 00:44:48 +0800

    update

--- fetch_toutiao_to_json.py
+++ fetch_toutiao_to_json.py
@@ -1,12 +1,13 @@
 import json
+from datetime import datetime
 from utils import (
     server_conn
 )
 
-base_path = ''
+base_path = '/opt/data/company_announcement_pdf/json/'
 
 def get_announcement_info(year):
-    query_str = "select * from cninfo_companyreport where publishtime >　'{}-01-01' and publishtime < {}-01-01;"
+    query_str = "select * from cninfo_companyreport where publishtime>='{}-01-01' and publishtime<'{}-01-01';"
     query_str = query_str.format(year, year+1)
     announcements = server_conn.query(query_str)
     return announcements
@@ -19,14 +20,25 @@ def fill_json(announcement):
     report_type = announcement['report_type']
     market_type = announcement['market_type']
     meta = json.loads(announcement['meta'])
-
+    publishtime = publishtime.strftime('%Y-%m-%d')
     print(publishtime)
-
+    print(title)
+    print(meta['secName'])
     file_name = '[{}][{}][{}]'.format(
                     publishtime,
                     meta['secName'],
                     title
                 )
+    json_file_name = '{}{}\{}.pdf'.format(base_path,publishtime,file_name)
+    with open(json_file_name) as json_file:
+        data = json.load(json_file)
+    data['url'] = url
+    data['title'] = title
+    data['meta'] = meta
+    data['martket_type'] = market_type
+    data['company_code'] = company_code
+    with open(json_file_name, 'w') as f:
+        json.dump(data, f)
 
 
 def fill_json_data_by_year(year):
--- process_announcement_pdf.sh
+++ process_announcement_pdf.sh
@@ -7,10 +7,10 @@ s=/opt/data/company_announcement_pdf/report/
 dest=$2
 dest=/opt/data/company_announcement_pdf/json/
 y=$3
-y=2019-01-02
+y=2019
 
 for d in $s$y* ; do
 	day_source="$d"
 	day_dest="${d/$s/$dest}"    
-	python pdf2json/pdf2json.py -s $day_source -d $day_dest -n 2
+	python pdf2json/pdf2json.py -s $day_source -d $day_dest -n 30
 done
